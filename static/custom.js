8212fcc90fadb335875985e78d2a39da5f0c7552
nixtone <nixtone@nxt.ru>
2022-06-10 18:10:05 +0300

    header

--- static/custom.js
+++ static/custom.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	
+	// Слайдер в шапке
+	$("#promoSlider").slick({
+		autoplay: true,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 30000,
+		arrows: false,
+		dots: true
+	});
+
+
+
 });
